27bc08e940bf0dbc436f06058f8d850b6c1af782
Iori Ichinose <191180103@smail.nju.edu.cn>
2021-10-19 00:40:22 +0800

    add about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,47 @@
 <script setup lang="ts">
-import { NH2 } from 'naive-ui';
+import { NH1, NH2, NP, NLayout } from 'naive-ui';
 </script>
 
 <template>
   <n-h2 prefix="bar">About</n-h2>
+  <n-layout>
+    <div class="about-content">
+      <img class="about-logo" src="../assets/index-logo.jpg" />
+      <n-h1 class="title">NJU Mirror</n-h1>
+      <n-p class="about-text">A refreshed version of Nanjing University Mirror frontend</n-p>
+      <n-p class="about-text">Based on Vue3 + NaiveUI</n-p>
+      <n-p class="about-text">Created by Iori</n-p>
+    </div>
+  </n-layout>
 </template>
+
+<style scoped lang="less">
+.about-content {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  text-align: center;
+  justify-content: center;
+
+  .about-text {
+    font-size: 16px;
+    margin: 4px 0;
+  }
+
+  .about-logo {
+    border-radius: 24px;
+    height: 240px;
+    width: 240px;
+    margin: 24px;
+    align-self: center;
+    box-shadow: 8px 8px 8px rgba(0, 0, 0, 0.2);
+    -webkit-box-shadow: 8px 8px 8px rgba(0, 0, 0, 0.2);
+  }
+
+  .title {
+    line-height: 1;
+    font-size: 64px;
+    margin-bottom: 18px !important;
+  }
+}
+</style>
