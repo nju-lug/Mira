56bff91dfef3916551aebeada74987081604fc5d
Iori Ichinose <191180103@smail.nju.edu.cn>
2021-11-11 16:26:30 +0800

    add base url

--- src/configs.ts
+++ src/configs.ts
@@ -1,2 +1,2 @@
-export const ServerPrefix = '/configs/';
+export const ServerPrefix = import.meta.env.BASE_URL + 'configs/';
 export const MobileWidth = 750;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -69,7 +69,7 @@ const routes: RouteRecordRaw[] = [
 ];
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 });
 
