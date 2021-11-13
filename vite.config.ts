f102bc7361b0fa8005fc68e74c3e2fa5d503dcce
Iori Ichinose <191180103@smail.nju.edu.cn>
2021-11-13 22:07:07 +0800

    split chunks to improve performance

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx({
-    // options are passed on to @vue/babel-plugin-jsx
-  })]
+  plugins: [
+    vue(),
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
+    }),
+    visualizer()
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue': ['vue', 'vuex', 'vue-router'],
+          'utils': ['dayjs', 'marked']
+        }
+      }
+    }
+  }
 });
